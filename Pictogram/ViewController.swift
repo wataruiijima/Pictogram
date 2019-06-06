b2dbcf09ddf882d652326939d6c1e3bd2a5b66de
wataruiijima <wataru.iijima@gmail.com>
2019-06-06 15:13:57 +0900

    Initial Commit

---
+++ Pictogram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pictogram
+//
+//  Created by w-iijima on 2019/06/06.
+//  Copyright © 2019 w-iijima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
